8bbb2cfdfc4fd0dbde30d967e5bb7d4c80b597da
PHPRHiNOS <help@simplecssjs.com>
2023-05-26 09:25:05 -0400

    Update Main.js

--- app/Main.js
+++ app/Main.js
@@ -1,2 +1,18 @@
 import React from 'react'
-import ReactDom from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+
+function ExampleComponent() {
+  return (
+    <div>
+      <h1>This is our app!!!</h1>
+      <p>the sky is blue...</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<ExampleComponent />)
+
+if (module.hot) {
+  module.hot.accept()
+}
